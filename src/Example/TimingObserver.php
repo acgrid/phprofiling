be96a63d6ab74a27171c410e7ee6f078b36c0af1
acgrid <acgrid@gmail.com>
2016-05-23 18:14:23 +0800

    Implement Example Feature: Timing

---
+++ src/Example/TimingObserver.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * This file is part of the acgrid/PHProfiling.
+ * @package acgrid/PHProfiling
+ * @link http://www.github.com/acgrid/PHProfiling
+ * @copyright Copyright (c) 2016 acgrid
+ * @license This source file is subject to the MIT license that is bundled with this source code in the file LICENSE.
+ */
+
+namespace PHProfiling\Example;
+
+
+use PHProfiling\Item;
+use PHProfiling\Observer\PauseObserver;
+use PHProfiling\Observer\ResumeObserver;
+use PHProfiling\Observer\StartObserver;
+use PHProfiling\Observer\StopObserver;
+
+/**
+ * Class TimingObserver
+ * Provide microsecond level execution time measurement
+ * @package PHProfiling\Example
+ */
+class TimingObserver implements StartObserver, PauseObserver, ResumeObserver, StopObserver
+{
+    const FILED_START = 'start';
+    const FILED_PAUSED_AT = 'pause';
+    const FILED_RUNTIME = 'run';
+
+    /**
+     * @var float The timestamp on execution start
+     */
+    protected $start;
+
+    /**
+     * TimingObserver constructor.
+     * @param float $startTimestamp
+     */
+    public function __construct($startTimestamp = null)
+    {
+        if(is_numeric($startTimestamp)){
+            $this->start = $startTimestamp;
+        }elseif(isset($_SERVER['REQUEST_TIME_FLOAT'])){
+            $this->start = $_SERVER['REQUEST_TIME_FLOAT'];
+        }else{
+            $this->start = static::getTS();
+        }
+        $this->start = floatval($this->start);
+    }
+    
+    protected function getStart(Item $item)
+    {
+        return $item->getStatistic(self::class, self::FILED_START);
+    }
+    
+    protected function getPausedAt(Item $item)
+    {
+        return $item->getStatistic(self::class, self::FILED_PAUSED_AT);
+    }
+    
+    protected function getRuntime(Item $item)
+    {
+        return $item->getStatistic(self::class, self::FILED_RUNTIME);
+    }
+    
+    protected function setRuntime(Item $item, $runtime)
+    {
+        return $item->setStatistic(self::class, self::FILED_RUNTIME, $runtime);
+    }
+
+    /**
+     * @param Item $item
+     * @return mixed
+     */
+    public function onPause(Item $item)
+    {
+        $item->setStatistic(self::class, self::FILED_PAUSED_AT, $this->getT());
+    }
+
+    /**
+     * @param Item $item
+     * @return mixed
+     */
+    public function onResume(Item $item)
+    {
+        $this->setRuntime($item, $this->getRuntime($item) - ($this->getT() - $this->getPausedAt($item)))
+            ->deleteStatistic(self::class, self::FILED_PAUSED_AT);
+    }
+
+    /**
+     * @param Item $item
+     * @return mixed
+     */
+    public function onStart(Item $item)
+    {
+        $item->setStatistic(self::class, self::FILED_START, $this->getT())
+            ->setStatistic(self::class, self::FILED_RUNTIME, 0);
+    }
+
+    /**
+     * @param Item $item
+     * @return mixed
+     */
+    public function onStop(Item $item)
+    {
+        if($pausedAt = $this->getPausedAt($item)){
+            $runtime = $pausedAt - $this->getStart($item);
+        }else{
+            $runtime = $this->getT() - $this->getStart($item);
+        }
+        $this->setRuntime($item, $this->getRuntime($item) + $runtime);
+    }
+    
+    public function getT()
+    {
+        return static::getTS() - $this->start;
+    }
+
+    /**
+     * @return float
+     */
+    public static function getTS()
+    {
+        return microtime(true);
+    }
+}
---
+++ tests/Example/TimingTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * This file is part of the acgrid/PHProfiling.
+ * @package acgrid/PHProfiling
+ * @link http://www.github.com/acgrid/PHProfiling
+ * @copyright Copyright (c) 2016 acgrid
+ * @license This source file is subject to the MIT license that is bundled with this source code in the file LICENSE.
+ */
+
+namespace PHProfilingTest\Example;
+
+
+use PHProfiling\Example\TimingObserver;
+use PHProfiling\Item;
+use PHProfiling\Manager;
+
+class TimingTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Manager
+     */
+    protected $manager;
+
+    protected function setUp()
+    {
+        $prototype = new Item();
+        $prototype->attach(new TimingObserver());
+        $this->manager = new Manager($prototype);
+    }
+
+    public function testTiming()
+    {
+        $this->manager->namedStart('test');
+        usleep(500000);
+        $this->manager->pause();
+        usleep(750000);
+        $this->manager->resume();
+        usleep(500000);
+        $item = $this->manager->stop();
+        $this->assertEquals(1, $item->getStatistic(TimingObserver::class, TimingObserver::FILED_RUNTIME),
+            'Time is out of tolerance, check delta first.', 0.1);
+        $this->manager->namedStart('test2');
+        usleep(300000);
+        $this->manager->pause();
+        usleep(700000);
+        $item = $this->manager->stop();
+        $this->assertEquals(0.3, $item->getStatistic(TimingObserver::class, TimingObserver::FILED_RUNTIME),
+            'Time is out of tolerance, check delta first.', 0.1);
+    }
+}
