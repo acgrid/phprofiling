c83e77c92d47d91bc6a04efed5e517419b0be768
acgrid <acgrid@gmail.com>
2016-05-24 14:01:13 +0800

    Implement Example Feature: Trace the function of profiling endpoints

---
+++ src/Example/BacktraceObserver.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * This file is part of the acgrid/PHProfiling.
+ * @package acgrid/PHProfiling
+ * @link http://www.github.com/acgrid/PHProfiling
+ * @copyright Copyright (c) 2016 acgrid
+ * @license This source file is subject to the MIT license that is bundled with this source code in the file LICENSE.
+ */
+
+namespace PHProfiling\Example;
+
+
+use PHProfiling\Item;
+use PHProfiling\Observer\StartObserver;
+use PHProfiling\Observer\StopObserver;
+
+class BacktraceObserver implements StartObserver, StopObserver
+{
+    const FILED_START_POSITION = 'start';
+    const FILED_STOP_POSITION = 'stop';
+
+    const SUB_FILED_EXPRESSION = 'invoke';
+    const SUB_FILED_FILE = 'file';
+    const SUB_FILED_LINE = 'line';
+
+    /*
+     * self::collectBacktrace()
+     * self::onXXX()
+     * call_user_func
+     * Item::notify()
+     * StateXXX::XXX()
+     * Item::XXX()
+     * [Manager::XXX()]
+     */
+    const SKIP_LEVELS = 8;
+
+    /**
+     * @param Item $item
+     * @param string $field
+     * @param string $invoke
+     * @param string $file
+     * @param int $line
+     * @return $this
+     */
+    protected function writeStatistics(Item $item, $field, $invoke, $file, $line)
+    {
+        $item->setStatistic(static::class, $field, [
+            static::SUB_FILED_EXPRESSION => $invoke,
+            static::SUB_FILED_FILE => $file,
+            static::SUB_FILED_LINE => $line,
+        ]);
+        return $this;
+    }
+
+    protected function collectBacktrace()
+    {
+        $backtrace = array_slice(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, static::SKIP_LEVELS), 5);
+
+        // Followed by Public Accessible Facade, i.e Index 1
+        $topNamespace = strstr(__NAMESPACE__, '\\', true);
+        while($trace = array_shift($backtrace)){
+            if(!isset($trace['class']) || (isset($trace['class']) && (strstr($trace['class'], '\\', true) <> $topNamespace))) return $trace;
+        }
+        return null;
+    }
+
+    /**
+     * @param Item $item
+     * @return mixed
+     */
+    public function onStart(Item $item)
+    {
+        $trace = $this->collectBacktrace();
+        if($trace) $this->writeStatistics($item, static::FILED_START_POSITION,
+            (isset($trace['class']) && isset($trace['type']) ? $trace['class'] . $trace['type'] : '') . $trace['function'],
+            isset($trace['file']) ? $trace['file'] : '',
+            isset($trace['line']) ? $trace['line'] : '');
+    }
+
+    /**
+     * @param Item $item
+     * @return mixed
+     */
+    public function onStop(Item $item)
+    {
+        $trace = $this->collectBacktrace();
+        if($trace) $this->writeStatistics($item, static::FILED_STOP_POSITION,
+            (isset($trace['class']) && isset($trace['type']) ? $trace['class'] . $trace['type'] : '') . $trace['function'],
+            isset($trace['file']) ? $trace['file'] : '',
+            isset($trace['line']) ? $trace['line'] : '');
+    }
+
+}
---
+++ tests/Example/BacktraceTest.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * This file is part of the acgrid/PHProfiling.
+ * @package acgrid/PHProfiling
+ * @link http://www.github.com/acgrid/PHProfiling
+ * @copyright Copyright (c) 2016 acgrid
+ * @license This source file is subject to the MIT license that is bundled with this source code in the file LICENSE.
+ */
+
+namespace PHProfilingTest\Example;
+
+
+use PHProfiling\Example\BacktraceObserver;
+use PHProfiling\Item;
+use PHProfiling\Manager;
+use PHProfiling\Observer\FinishObserver;
+
+class BacktraceTest extends \PHPUnit_Framework_TestCase implements FinishObserver
+{
+    /**
+     * @var Manager
+     */
+    protected $manager;
+
+    /**
+     *
+     */
+    protected function setUp()
+    {
+        $item = new Item();
+        $item->attach($this);
+        $item->attach(new BacktraceObserver());
+        $this->manager = new Manager($item);
+    }
+
+    /**
+     * @param Item $item
+     * @return mixed
+     */
+    public function onFinish(Item $item)
+    {
+        $this->assertArrayHasKey(BacktraceObserver::class, $item->getStatistics());
+    }
+
+    protected function innerFunction()
+    {
+        $trace = $this->manager->stop()->refScopeStatistic(BacktraceObserver::class);
+        $this->assertSame('PHProfilingTest\Example\BacktraceTest->innerFunction',
+            $trace[BacktraceObserver::FILED_STOP_POSITION][BacktraceObserver::SUB_FILED_EXPRESSION]);
+    }
+
+    public function testInFunction()
+    {
+        $this->manager->namedStart('1');
+        $trace = $this->manager->stop()->refScopeStatistic(BacktraceObserver::class);
+        $this->assertSame('PHProfilingTest\Example\BacktraceTest->testInFunction',
+            $trace[BacktraceObserver::FILED_START_POSITION][BacktraceObserver::SUB_FILED_EXPRESSION]);
+        $this->assertEquals($trace[BacktraceObserver::FILED_START_POSITION], $trace[BacktraceObserver::FILED_STOP_POSITION]);
+        $this->manager->namedStart('2');
+        $this->innerFunction();
+    }
+
+    public function testInClosure()
+    {
+        $finish = function(Item $item){
+            $trace = $item->stop()->refScopeStatistic(BacktraceObserver::class);
+            $this->assertSame('PHProfilingTest\Example\BacktraceTest->testInClosure',
+                $trace[BacktraceObserver::FILED_START_POSITION][BacktraceObserver::SUB_FILED_EXPRESSION]);
+            $this->assertSame('PHProfilingTest\Example\BacktraceTest->PHProfilingTest\Example\{closure}',
+                $trace[BacktraceObserver::FILED_STOP_POSITION][BacktraceObserver::SUB_FILED_EXPRESSION]);
+        };
+        $finish($this->manager->namedStart('3'));
+    }
+}
